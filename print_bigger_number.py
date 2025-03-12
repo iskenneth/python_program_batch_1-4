f84eeae5d493043e007d195a852c7835f8aad6d3
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 16:34:59 +0800

    added the if else statement

--- print_bigger_number.py
+++ print_bigger_number.py
@@ -3,7 +3,14 @@ while True:
     try:
         num1= float(input("Enter a number:"))
         num2= float(input("Enter a number:"))
+#Check what is the bigger number (Num1>Num2 or Num2>Num1)
+        if num1 > num2: 
+              print("Bigger number:", num1)
+        elif num2 > num1:
+               print("Bigger number:", num2)#print the bigger number
+        else:
+               print("Both numbers are equal")
+        break
     except ValueError:
         print("Error")
-#Check what is the bigger number (Num1>Num2 or Num2>Num1)
-#print the bigger number
+
