06eed54a5e1d3570e4e901f19800281901efbc7f
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 18:21:16 +0800

    Input multipliers

--- product_of_two_numbers.py
+++ product_of_two_numbers.py
@@ -1,6 +1,8 @@
 #Input two numbers
 while True:
     try:
+        num1 = float(input("Enter a number:"))
+        num2 = float(input("Enter a number:"))
     except ValueError:
         print("Invalid Character")
 # multiply the input numbers
