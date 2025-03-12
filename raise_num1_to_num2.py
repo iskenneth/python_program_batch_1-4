6a80d06ed415282f81910ce6191c4041c3813183
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 18:42:05 +0800

    Input numbers

--- raise_num1_to_num2.py
+++ raise_num1_to_num2.py
@@ -1,6 +1,8 @@
 #Input two numbers
 while True:
     try:
+        num1=float(input("Enter your first number:"))
+        num2=float(input("Enter your second number:"))
     except ValueError:
         print("ERROR!!!")
 #Raise the first number to second number
