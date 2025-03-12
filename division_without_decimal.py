fc47a26702d251ea0a3c8d429d05813c52af5111
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 22:26:06 +0800

    input numbers

--- division_without_decimal.py
+++ division_without_decimal.py
@@ -1,3 +1,9 @@
 #input two numbers
+while True:
+    try:
+        num1 = float(input("Enter a number:"))
+         num2 = float(input("Enter a number:"))
+    except ValueError:
+        print("ERRORR!!")"
 #Divide the input numbers
 #print the quotient without the decimal
