0c0b06c66f9565071afae96145a5f8a347a42b40
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 20:29:33 +0800

    Input a number

--- print_smallest_number.py
+++ print_smallest_number.py
@@ -1,3 +1,9 @@
 #Input two numbers
+while True:
+    try:
+        num1 = float(input("Enter a number:"))
+        num2 = float(input("Enter a number:"))
+    except ValueError:
+        print("ERRORRR!!"")
 #compare which is the smallest number
 #print the smallest number
