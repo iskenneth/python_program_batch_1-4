9978a490aba7047024f2e46e2f946b5b9c411ba5
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 20:55:55 +0800

    input numners

--- subtraction_of_two_numbers.py
+++ subtraction_of_two_numbers.py
@@ -1,3 +1,9 @@
 #Input two numbers
+while True:
+    try:
+        num1 = float(input("Enter a number:"))
+         num2 = float(input("Enter a number:"))
+    except ValueError:
+        print("ERRORRRR!!")"
 #suntract input
 #print result
