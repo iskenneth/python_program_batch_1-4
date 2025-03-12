0f242b21350c9d2b048a17a84d1da1ee9adc9d19
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 16:55:01 +0800

    Inut two numbers

--- print_equal_numbers.py
+++ print_equal_numbers.py
@@ -1,3 +1,8 @@
 #Input two numbers
+while True:
+    try:
+        num1=float(input("Enter a number:"))
+        num2=float(input("Enter a number:"))
+    
 #Verify if it is equal numbers
 #print the confirmation
