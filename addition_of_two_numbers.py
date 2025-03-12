bb6e2a69a0d5613e405acda0f4a620902ce437c2
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 18:14:07 +0800

    sum of two input numbers

--- addition_of_two_numbers.py
+++ addition_of_two_numbers.py
@@ -1,9 +1,13 @@
-#Input to numbers
+
 while True:
     try:
-        num1=float(input("Enter your first number:"))
-        num2=float(input("Enter your second number:"))
+        num1 = float(input("Enter your first number:"))
+        num2 = float(input("Enter your second number:")) #Input two numbers
+        
+        sum = num1  +  num2  #Add the 2 numbers
+        print(f"The Sum of {num1} and {num2} are {sum}") #Print result
+        break  
+        
     except ValueError:
         print("Invalid Character")
-#Add the input numbers
-#Print result
+
