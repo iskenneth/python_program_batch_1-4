4440133ae447c1bf1524d9cc9b5b0026b8f56a9a
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 20:51:24 +0800

    checking if numbers is not equal

--- print_input_is_not_equal.py
+++ print_input_is_not_equal.py
@@ -3,7 +3,12 @@ while True:
     try:
          num1 = float(input("Enter a number:"))
          num2 = float(input("Enter a number:"))
+         
+         #check if input numbers is not equal
+         if num1 != num2:
+             print(f"{num1} and {num2} are NOT EQUAL")                 #print findings
+         else:
+             print(f"{num1} and {num2} are EQUAL") #print findings
     except ValueError:
          print("ERRORRR!!")
-#check if input number is not equal
-#print findings
+
