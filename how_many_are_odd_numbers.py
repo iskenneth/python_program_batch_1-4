3012f8dc48e972621ed8331c400547017daf526f
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 19:12:55 +0800

    pseudocode

---
+++ how_many_are_odd_numbers.py
@@ -0,0 +1,6 @@
+#Input numbers
+#store numbers in list
+# add odd number counter
+#Check each  inputs in the list: if it is a odd number increase the counter
+#print how many odd numvers are in the list
+     
