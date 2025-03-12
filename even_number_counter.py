21a8b599ca5d36cb1cc24db56fda76dfb664a41d
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 23:22:02 +0800

    print the result

--- even_number_counter.py
+++ even_number_counter.py
@@ -4,11 +4,11 @@ even_number = 0
 while count < 10:
     try: 
         num = float(input(f"Enter number {count + 1}:"))
-        if num % 2 == 0:
-            even_number +=1
-        count += 1       
+        if num % 2 == 0: # check each numbers if divisible by 2
+            even_number +=1 #count the numbers
+        count += 1     
     except ValueError:
         print("Error")
-#count the numbers
-# check each numbers if divisible by 2
-#Print how many is even numbers
+print("Count of even numbers:", even_number) #Print how many is even numbers       
+
+
