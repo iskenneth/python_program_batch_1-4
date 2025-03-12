742bb5bcb70a9df15a536fd292c99237e6536bf1
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 23:15:37 +0800

    check if number is even

--- even_number_counter.py
+++ even_number_counter.py
@@ -1,8 +1,12 @@
 #Input 10 numbers
 count = 0
+even_number = 0
 while count < 10:
     try: 
         num = float(input(f"Enter number {count + 1}:"))
+        if num % 2 == 0:
+            even_number +=1
+        count += 1       
     except ValueError:
         print("Error")
 #count the numbers
