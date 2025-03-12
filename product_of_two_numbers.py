179894999682d35bb9c7363300c5bda62d452717
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 18:24:51 +0800

    Proceed to multiplications and printing result

--- product_of_two_numbers.py
+++ product_of_two_numbers.py
@@ -3,7 +3,10 @@ while True:
     try:
         num1 = float(input("Enter a number:"))
         num2 = float(input("Enter a number:"))
+
+# multiply the input numbers
+        product = num1 * num2 
+        print(f"The product of {num1} and num {num2} are {product}") #print result
     except ValueError:
         print("Invalid Character")
-# multiply the input numbers
-#print result
+
