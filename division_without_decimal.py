a65b74c6158952d507cdce5d73bc96853b67e3ce
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 22:32:56 +0800

    insert the operation and remove the decimals

--- division_without_decimal.py
+++ division_without_decimal.py
@@ -3,7 +3,12 @@ while True:
     try:
         num1 = float(input("Enter a number:"))
         num2 = float(input("Enter a number:"))
+        
+        #Divide the input numbers
+        quotient = num1  // num2
+        result = int(quotient) #remove the decimals
+        print("Quotient without decimal:", result)#print the quotient without the decimal
+        break
     except ValueError:
-        print("ERRORR!!")"
-#Divide the input numbers
-#print the quotient without the decimal
+        print("ERRORR!!")
+
