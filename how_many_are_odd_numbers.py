5640f49bba034b02f01e30fb4124db914cd824cf
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 19:15:40 +0800

    list storage for odd and inputs

--- how_many_are_odd_numbers.py
+++ how_many_are_odd_numbers.py
@@ -1,6 +1,8 @@
-#Input numbers
-#store numbers in list
-# add odd number counter
+
+odd_numbers = 0 # add odd number counter
+inputs = 0 #store numbers in list
+
+
 #Check each  inputs in the list: if it is a odd number increase the counter
 #print how many odd numvers are in the list
      
