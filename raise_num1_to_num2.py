4c8a182d108cb94243396bd8baac1e9a1679eed6
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 18:47:51 +0800

    I input the operation

--- raise_num1_to_num2.py
+++ raise_num1_to_num2.py
@@ -3,7 +3,11 @@ while True:
     try:
         num1 = float(input("Enter your first number:"))
         num2 = float(input("Enter your second number:"))
+
+#Raise the first number to second number
+        result = num1 ** num2
+        print("1st number is raised to 2nd number = ", result)  #print result
+        break                             
     except ValueError:
         print("ERROR!!!")
-#Raise the first number to second number
-#print result
+
