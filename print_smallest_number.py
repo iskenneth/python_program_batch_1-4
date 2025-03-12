fcf7eb75f074960f1084a806605b3a7ef19d812a
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 20:37:31 +0800

    If else statement to determine which is smaller

--- print_smallest_number.py
+++ print_smallest_number.py
@@ -3,7 +3,15 @@ while True:
     try:
         num1 = float(input("Enter a number:"))
         num2 = float(input("Enter a number:"))
-    except ValueError:
-        print("ERRORRR!!"")
+
 #compare which is the smallest number
-#print the smallest number
+        if num1 < num2:
+            print(f"{num1} is smaller") #print the smallest number
+        elif num2 < num1:
+            print(f"{num2} is smaller") #print the smallest number
+        else:
+            print("Numbers are equal")
+        break               
+    except ValueError:
+        print("ERRORRR!!")
+
