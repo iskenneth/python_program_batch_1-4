9b749ec510ac890fe2b80b9095aa3dc374f83041
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 23:38:46 +0800

    input numbers

--- numbers_between_input_numbers.py
+++ numbers_between_input_numbers.py
@@ -1,2 +1,8 @@
 #Ask user to input 2 numbers
+while True:
+    try:
+        num1 = float(input("Enter a number:"))
+        num2 = float(input("Enter a number:"))
+    except ValueError:
+        print("Error!!!")
 #Print numbers in between input numbers
