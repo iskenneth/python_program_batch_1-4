88004a0490ebe33e638fa929017c261f46576f71
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 22:46:16 +0800

    Applying modulus division

--- modulus_division.py
+++ modulus_division.py
@@ -3,7 +3,11 @@ while True:
     try:
         num1 = float(input("Enter a number:"))
         num2 = float(input("Enter a number:"))
+        
+        # Perform modulus division on num1 to num 2
+        result = num1 % num2
+        print("Remainder:", result) # Print the result
+        break
     except ValueError:
-        print("INVALID CHARACTER")"
-# Perform modulus division on num1 to num 2
-# Print the result
+        print("INVALID CHARACTER")
+
