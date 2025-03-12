96f5d68a4b2a5f800e3bf61ecba1d447e45c48c0
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 18:31:13 +0800

    input the divisor and dividend

--- quotient_of_two_numbers.py
+++ quotient_of_two_numbers.py
@@ -1,6 +1,9 @@
 #Ask the user to Input two numbers
 while True:
     try:
+        num1 = float(input("Enter a number:"))
+        num2 = float(input("Enter a number:"))
+
     except ValueError:
         print("ERROR!!!")
 #Proceed to division
