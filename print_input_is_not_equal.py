27f5f2dccac67b671bedec2acfe3a52423b65fae
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 20:45:52 +0800

    input numbers

--- print_input_is_not_equal.py
+++ print_input_is_not_equal.py
@@ -1,3 +1,9 @@
 #input numbers
+while True:
+    try:
+         num1 = float(input("Enter a number:"))
+         num2 = float(input("Enter a number:"))
+    except ValueError:
+         print("ERRORRR!!")
 #check if input number is not equal
 #print findings
