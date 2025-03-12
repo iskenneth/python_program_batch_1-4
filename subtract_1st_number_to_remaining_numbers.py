77f6f3c2f7dcf98534cc7cd4db82ea245cee811b
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 22:55:47 +0800

    Storing all input numbers

--- subtract_1st_number_to_remaining_numbers.py
+++ subtract_1st_number_to_remaining_numbers.py
@@ -1,4 +1,17 @@
 #Input 10 numbers
-#Get the first number
-# Subtract the first number to the rest
+while True: 
+    try:
+        numbers : [] #store numbers
+        for number in range(10):
+            num = float(input(f"Enter {number + 1}"))
+            numbers.append(num) #Get the first number
+        
+        # Subtract the first number to the rest
+         
+         
+            
+    except ValueError:
+        print("ERROR 404!!")    
+
+
 # Print result
