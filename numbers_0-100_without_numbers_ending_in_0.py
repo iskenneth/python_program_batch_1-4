ba624394794a69d26246ddc1ab4845f79c6821fe
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 20:24:32 +0800

    check number is ending with zero

--- numbers_0-100_without_numbers_ending_in_0.py
+++ numbers_0-100_without_numbers_ending_in_0.py
@@ -1,4 +1,5 @@
 #loop from 0-100
-for numbers in range (0-101)
-# check if number is not divisible by 10
+for numbers in range (0-101):
+    if numbers % != 10: # check if number is not divisible by 10
+        print(numbers, end=" ")
 #print numbers not divisible to 10
