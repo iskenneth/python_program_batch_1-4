2ebb3701b4613f9819203e777040df1984c62cb6
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 23:00:07 +0800

    Print the result

--- subtract_1st_number_to_remaining_numbers.py
+++ subtract_1st_number_to_remaining_numbers.py
@@ -3,15 +3,14 @@ while True:
     try:
         numbers : [] #store numbers
         for number in range(10):
-            num = float(input(f"Enter {number + 1}"))
+            num = float(input(f"Enter {number + 1}:"))
             numbers.append(num) #Get the first number
         
         # Subtract the first number to the rest
-         result = numbers[0] - sum(numbers[1:])
-         
-            
+        result = numbers[0] - sum(numbers[1:])
+        print ("Diffirence: ", result) # Print result
+        break
     except ValueError:
         print("ERROR 404!!")    
 
 
-# Print result
