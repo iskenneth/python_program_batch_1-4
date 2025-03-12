e91ea74613d61246b0eb782ad7f754f1b9e1db13
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 19:18:31 +0800

    Input numbers

--- how_many_are_odd_numbers.py
+++ how_many_are_odd_numbers.py
@@ -1,6 +1,12 @@
 
 odd_numbers = 0 # add odd number counter
 inputs = 0 #store numbers in list
+#Input numbers
+while inputs < 10:
+    try:
+        num = float(input(f"Enter number {inputs + 1}:")) 
+    except VablueError:
+        print("ERROR!!")
 
 
 #Check each  inputs in the list: if it is a odd number increase the counter
