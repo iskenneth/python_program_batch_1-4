de2b524425e379365927c5ff0580aa472a74b672
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 22:40:18 +0800

    input number

--- modulus_division.py
+++ modulus_division.py
@@ -1,3 +1,9 @@
 #Input numbers
+while True:
+    try:
+        num1 = float(input("Enter a number:"))
+        num2 = float(input("Enter a number:"))
+    except ValueError:
+        print("INVALID CHARACTER")"
 # Perform modulus division on num1 to num 2
 # Print the result
