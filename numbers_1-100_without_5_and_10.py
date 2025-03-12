afb13f76cddcc6466a3c062740e5785a2c0cbaa5
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-12 23:34:23 +0800

    remove numbers ending with 5 and 0

--- numbers_1-100_without_5_and_10.py
+++ numbers_1-100_without_5_and_10.py
@@ -1,3 +1,4 @@
 #Range from 1-101
-#check if the numbers is not visible to 10 and 5
-# print the numbers if not divisible by 10 and 5
+for numbers in range (1,100):
+    if numbers % 5 != 0 and numbers % 10 != 0: #check if the numbers is not visible to 10 and 5
+        print(numbers, end=" ") # print the numbers if not divisible by 10 and 5
