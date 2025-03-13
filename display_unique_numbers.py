d1a0e5dbd918f14badcd7d992ff1b898216468c7
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-13 09:47:11 +0800

    add functions to store numbers

--- display_unique_numbers.py
+++ display_unique_numbers.py
@@ -1,9 +1,13 @@
 #Ask user to input 10 numbers
+numbers = [] #store the numbers
+count = 0
 while count < 10:
     try:
-        num = float(input(f"Enter a number{count+1}:""))
+        num = float(input(f"Enter a number{count + 1}:"))
+        numbers.append(num)
+        count += 1
     except ValueError:
         print("Enter a valid character")
-#store the numbers
+
 #If number is unique keep it
 #print unique numbers
