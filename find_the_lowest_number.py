822e765806cd952be2dcabfc6ab7a5b675ddc747
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-13 12:19:11 +0800

    ValueError code

--- find_the_lowest_number.py
+++ find_the_lowest_number.py
@@ -1,4 +1,9 @@
 #Ask the user to enter numbers.
+while True:
+    try:
+    except ValueError:
+        print("Stopping the program...")
+        break
 #Store all valid numbers.
 #Stop when an invalid input is entered.
 #Display the lowest number from the list.
