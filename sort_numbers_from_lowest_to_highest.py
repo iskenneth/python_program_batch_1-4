a463d6a4d735bffdf694495b3f34e9e5491fb0db
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-13 12:37:12 +0800

    input numbers

--- sort_numbers_from_lowest_to_highest.py
+++ sort_numbers_from_lowest_to_highest.py
@@ -1,6 +1,7 @@
 #Ask the user to enter numbers.
 while True:
     try:
+        num = float(input("Enter a number (Input invalid character to stop):"))
     except ValueError:
         print("Displaying sorted numbers")
         break
