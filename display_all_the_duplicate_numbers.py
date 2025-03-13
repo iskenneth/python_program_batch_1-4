6339ccdaf8ed810fd039716e681f5e8b30ad7747
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-13 13:41:48 +0800

    list of numbers

--- display_all_the_duplicate_numbers.py
+++ display_all_the_duplicate_numbers.py
@@ -1,10 +1,11 @@
 print("Enter 10 numbers")
+list_numbers = [] # storage of numbers
 #Ask the user to enter 10 numbers.
 for number in range (10)
     try:
         num = float(input("Enter number:"))
+        list_numbers.append(num) #Store the numbers in a list.
     except ValueError:
         print ("Invalid Character")
-#Store the numbers in a list.
 #Find numbers that appear more than once.
 #Display only the duplicate numbers (each number is shown once).
