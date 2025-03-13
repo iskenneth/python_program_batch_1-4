cedb86bf745c2c403d58513a47c5a884968a0407
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-13 12:35:59 +0800

    ValueError function

--- sort_numbers_from_lowest_to_highest.py
+++ sort_numbers_from_lowest_to_highest.py
@@ -1,4 +1,10 @@
 #Ask the user to enter numbers.
+while True:
+    try:
+    except ValueError:
+        print("Displaying sorted numbers")
+        break
+        
 #Store all valid numbers.
 #Stop when an invalid input is entered.
 #Sort the numbers in ascending order.
