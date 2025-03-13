c905e9e33d1257c05a3bf93d61f34c932cbfce48
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-13 10:05:16 +0800

    Storing inputs

--- display_numbers_first_occurence.py
+++ display_numbers_first_occurence.py
@@ -1,11 +1,12 @@
 # Input 10 numbers
 count = 0
+numbers = []
 while count < 10:
     try:
         num = float(input(f"Enter number{count +1}:"))
+        numbers.append(num) #Store the numbers
         count += 1
     except ValueError:
         print("Error!!")
-#Store the numbers
 #Display numbers 
 #If numbers have duplicate print only the first one
