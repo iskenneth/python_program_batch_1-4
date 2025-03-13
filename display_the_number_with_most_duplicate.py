0729f8cb4e7bb1463421b82086079f51da5d2d81
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-13 13:58:46 +0800

    store input valid numbers

--- display_the_number_with_most_duplicate.py
+++ display_the_number_with_most_duplicate.py
@@ -1,11 +1,13 @@
 #Ask the user to enter numbers continuously.
 print("Good day, this program will stop when you input invalid character")
+numbers = [] #input storage
 while True:
     try:
         num = float(input("Enter a number: "))
+        numbers.appens(num) #store number
     except ValueError:
         print ("Stopping the program....")
-        break
-#Stop when the input is invalid.
+        break #Stop when the input is invalid.
+
 #Count how many times each number appears.
 #Find and display the number that appears the most.
