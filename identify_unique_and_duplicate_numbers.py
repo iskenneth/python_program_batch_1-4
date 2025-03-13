336757885ee31fb1661ff3afe6c035541d29653e
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-13 11:58:52 +0800

    checking if it is duplicate or unique

--- identify_unique_and_duplicate_numbers.py
+++ identify_unique_and_duplicate_numbers.py
@@ -4,10 +4,16 @@ print("Enter a number, (to stop enter invalid character):")
 while True:
     try:
         num = float(input("Number: "))
+        
+        #if input is unique print "unique" and if input is already in store numbers print "duplicate"
+        if num in numbers:
+            print ("Duplicate")
+        else:
+            print("Unique")        
         numbers.append(num)
     except ValueError:
         print("Stoping Program")
         break
 
-#if input is unique print "unique" and if input is already in store numbers print "duplicate"
+
 #repeat until the user input invalid number to stop
