97bb295bdc65626d5184edf21e4ae735b9ae731e
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-13 11:52:19 +0800

    Value error function

--- identify_unique_and_duplicate_numbers.py
+++ identify_unique_and_duplicate_numbers.py
@@ -1,4 +1,10 @@
 #Ask user to input numbers
+print("Enter a number, (to stop enter invalid character):")
+while True:
+    try:
+    except ValueError:
+        print("Stoping Program")
+        break
 #store the input
 #if input is unique print "unique" and if input is already in store numbers print "duplicate"
 #repeat until the user input invalid number to stop
