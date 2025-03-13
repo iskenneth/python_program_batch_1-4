04dc8dd4098d86d112644f5ed77922df55032252
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-13 10:16:53 +0800

    pseudocode

---
+++ identify_unique_and_duplicate_numbers.py
@@ -0,0 +1,4 @@
+#Ask user to input numbers
+#store the input
+#if input is unique print "unique" and if input is already in store numbers print "duplicate"
+#repeat until the user input invalid number to stop
