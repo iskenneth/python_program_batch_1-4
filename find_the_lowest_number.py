1318e9add7be0465aba8aebf0a44325d0f9eabdf
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-13 12:23:31 +0800

    store valid numbers

--- find_the_lowest_number.py
+++ find_the_lowest_number.py
@@ -1,10 +1,12 @@
 #Ask the user to enter numbers.
+numbers = [] #storage numbers
 while True:
     try:
         num = float(input("Number (input invalid number to stop the program):"))
+        numbers.append(num) #Store all valid numbers.
     except ValueError:
         print("Stopping the program...")
         break
-#Store all valid numbers.
+
 #Stop when an invalid input is entered.
 #Display the lowest number from the list.
