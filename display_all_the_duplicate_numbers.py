156964fc81906e232ff8df915c09eaf76c7777f4
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-13 13:38:31 +0800

    Input number and range

--- display_all_the_duplicate_numbers.py
+++ display_all_the_duplicate_numbers.py
@@ -1,4 +1,10 @@
+print("Enter 10 numbers")
 #Ask the user to enter 10 numbers.
+for number in range (10)
+    try:
+        num = float(input("Enter number:"))
+    except ValueError:
+        print ("Invalid Character")
 #Store the numbers in a list.
 #Find numbers that appear more than once.
 #Display only the duplicate numbers (each number is shown once).
