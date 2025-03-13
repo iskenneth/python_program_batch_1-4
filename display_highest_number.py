5bbbdff00335a21aa163ed8e6f42b641ed76a30e
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-13 14:15:29 +0800

    Except value error to stop program

--- display_highest_number.py
+++ display_highest_number.py
@@ -1,3 +1,10 @@
+print("to stop the program input invalid character")
 #Ask the user to enter numbers continuously.
+while True:
+    try:        
+    except ValueError:
+        print("Stopping the program...")
+        break
+
 #Stop when the input is invalid.
 #Find and display the highest number entered.
