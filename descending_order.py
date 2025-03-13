41959559389f017af61256779bf6b8390896e3e2
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-13 14:42:56 +0800

    inout numbers

--- descending_order.py
+++ descending_order.py
@@ -1,6 +1,7 @@
 #Ask the user to enter numbers continuously.
 while True:
     try:
+        num = float(input("Enter a number (to stop enter a letter): "))
     except ValueError:
         print("Stopping program...")
         break #Stop when the input is invalid.
