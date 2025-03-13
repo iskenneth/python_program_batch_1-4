07bd719114533c5ab4ce95cbba13e42162071d7a
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-13 11:56:14 +0800

    numbers appen

--- identify_unique_and_duplicate_numbers.py
+++ identify_unique_and_duplicate_numbers.py
@@ -4,6 +4,7 @@ print("Enter a number, (to stop enter invalid character):")
 while True:
     try:
         num = float(input("Number: "))
+        numbers.append(num)
     except ValueError:
         print("Stoping Program")
         break
