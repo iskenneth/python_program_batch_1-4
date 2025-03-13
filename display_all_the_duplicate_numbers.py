30b7b54a7ace4c2a40e8e6bdd286169f8a6e646a
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-13 13:45:29 +0800

    Finding numbers with duplicates

--- display_all_the_duplicate_numbers.py
+++ display_all_the_duplicate_numbers.py
@@ -1,11 +1,14 @@
 print("Enter 10 numbers")
 list_numbers = [] # storage of numbers
 #Ask the user to enter 10 numbers.
-for number in range (10)
+for number in range (10):
     try:
         num = float(input("Enter number:"))
         list_numbers.append(num) #Store the numbers in a list.
     except ValueError:
         print ("Invalid Character")
+        
 #Find numbers that appear more than once.
+duplicate = set(num for num in list_numbers if list_numbers.count(num) > 1)
+
 #Display only the duplicate numbers (each number is shown once).
