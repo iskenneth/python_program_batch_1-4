18f921216ec97afb04af68cad481d4b3406842cf
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-13 09:42:22 +0800

    Input numbers

--- display_unique_numbers.py
+++ display_unique_numbers.py
@@ -1,4 +1,9 @@
 #Ask user to input 10 numbers
+while count < 10:
+    try:
+        num = float(input(f"Enter a number{count+1}:""))
+    except ValueError:
+        print("Enter a valid character")
 #store the numbers
 #If number is unique keep it
 #print unique numbers
