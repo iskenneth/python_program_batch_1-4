ec681f91de9c38a6e05e4cf526799e6c788dddd5
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-13 14:53:25 +0800

    input numbers

--- average_calculator.py
+++ average_calculator.py
@@ -1,6 +1,7 @@
 #Ask the user to enter numbers continuously.
 while True:
     try:
+        num = float(input("Enter a number (Input invalid character to stop): "))
     except ValueError:
         print("......")
         break #Stop when the input is invalid.
