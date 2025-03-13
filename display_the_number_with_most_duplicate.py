af74b3f5ca33d18c1eb707ee2a13b4a92671e7b4
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-13 13:52:36 +0800

    Added ValueError function to stop the program

--- display_the_number_with_most_duplicate.py
+++ display_the_number_with_most_duplicate.py
@@ -1,4 +1,10 @@
 #Ask the user to enter numbers continuously.
+print("Good day, this program will stop when you input invalid character")
+while True:
+    try:
+    except ValueError:
+        print ("Stopping the program....")
+        break
 #Stop when the input is invalid.
 #Count how many times each number appears.
 #Find and display the number that appears the most.
