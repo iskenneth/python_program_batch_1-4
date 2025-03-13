7bb6bd569cbe50f081f39ff69239d0dc7d99c022
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-13 12:38:57 +0800

    storing all valid input numbers

--- sort_numbers_from_lowest_to_highest.py
+++ sort_numbers_from_lowest_to_highest.py
@@ -1,12 +1,14 @@
 #Ask the user to enter numbers.
+numbers = [] #storage
 while True:
     try:
         num = float(input("Enter a number (Input invalid character to stop):"))
+        numbers.append(num) #Store all valid numbers.
     except ValueError:
         print("Displaying sorted numbers")
-        break
+        break #Stop when an invalid input is entered.
         
-#Store all valid numbers.
-#Stop when an invalid input is entered.
+
+
 #Sort the numbers in ascending order.
 #Display the sorted numbers
