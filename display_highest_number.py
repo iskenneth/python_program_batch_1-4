89aca782c51253dd0ad8a5e5bf4f11cba8889eba
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-13 14:18:30 +0800

    store input numbers

--- display_highest_number.py
+++ display_highest_number.py
@@ -1,11 +1,13 @@
 print("to stop the program input invalid character")
 #Ask the user to enter numbers continuously.
+numero = [] #number storage
 while True:
     try:
-        num = float(input("Enter a number: "))        
+        num = float(input("Enter a number: "))  
+        numero.append(num)  #store numbers in list
     except ValueError:
         print("Stopping the program...")
-        break
+        break #Stop when the input is invalid.
 
-#Stop when the input is invalid.
+
 #Find and display the highest number entered.
