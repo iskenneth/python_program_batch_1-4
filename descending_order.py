a69ca8f22679b9b9f55c214252dcb29c994b534a
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-13 14:44:46 +0800

    Store inputs

--- descending_order.py
+++ descending_order.py
@@ -1,7 +1,9 @@
 #Ask the user to enter numbers continuously.
+inputs = [ ] #input storage
 while True:
     try:
         num = float(input("Enter a number (to stop enter a letter): "))
+        inputs.append(num) #store inputs
     except ValueError:
         print("Stopping program...")
         break #Stop when the input is invalid.
